c9ca00841afee7594a0179e8e8483ace87f28f42
KolteAkash <akashkolte240899@gmail.com>
2023-03-26 22:42:23 +0530

    Updated occurance.java

--- occurance.java
+++ occurance.java
@@ -1,20 +1,28 @@
 import java.util.HashMap;
 import java.util.Scanner;
-
-public class occurance {
-    public static void main(String[] args) {
+public class occurance 
+{
+    
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String str = sc.nextLine();
         HashMap<Character, Integer> charCount = new HashMap<>();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (charCount.containsKey(str.charAt(i))) {
-                int count = charCount.get(str.charAt(i));
-                charCount.put(str.charAt(i), ++count);
-            } else {
-                charCount.put(str.charAt(i), 1);1
+        for (char c : str.toCharArray()) 
+        {
+            char ch = Character.toLowerCase(c);
+            if (charCount.containsKey(ch)) 
+            {
+                int count = charCount.get(ch);
+                charCount.put(ch, ++count);
+            } else 
+            {
+                charCount.put(ch, 1);
             }
         }
         System.out.println(charCount);
     }
+
+ 
 }
